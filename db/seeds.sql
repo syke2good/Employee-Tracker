07cfe2337861d1dabc79dab3dea87083bf2ccb6f
Aakash Bhandari <akashbhandari930@gmail.com>
2021-12-22 22:02:46 -0800

    added db and function codes

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
-INSERT INTO department (id, name)
+INSERT INTO department (name)
 VALUES 
-(1, 'Programming'),
-(2, 'Finance & Accounting'),
-(3, 'Sales & Marketing'),
-(4, 'Operations');
+('Programming'),
+('Finance & Accounting'),
+('Sales & Marketing'),
+('Operations');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
@@ -19,11 +19,11 @@ VALUES
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
-('Mark', 'Miller', 2, null),
-('Devin', 'Anderson', 1, 1),
-('Mary', 'Brown', 4, null),
-('Ashley', 'Jones', 3, 3),
-('Tyler', 'Moore', 6, null),
-('Ana', 'Sanchez', 5, 5),
-('Lewis', 'Allen', 7, null),
-('Katherine', 'Green', 8, 7);
+('Natsu', 'Dragneel', 2, null),
+('Kurosaki', 'Ichigo', 1, 1),
+('Uzumaki', 'Naruto', 4, null),
+('Heartfilia', 'Lucy', 3, 3),
+('Ishigami', 'Senku', 6, null),
+('Roronoa', 'Zoro', 5, 5),
+('Hancock', 'Boa', 7, null),
+('Scarlet', 'Erza', 8, 7);
---
+++ server.js
@@ -0,0 +1,148 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // MySQL username,
+        user: 'root',
+        // TODO: Add MySQL password here
+        password: '-Akash1998',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+);
+
+db.connect(err => {
+    if (err) throw err
+    main()
+});
+
+
+
+function main() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'what would like to do?',
+            name: 'choice',
+            choices: [
+                'view all departments?',
+                'view all roles?',
+                'view all employees?',
+                'add a department?',
+                'add a role?',
+                'add an employee?',
+                'update an employee role?',
+            ]
+        }
+    ]).then(function (val) {
+        switch (val.choice) {
+            case 'view all departments?':
+                viewAlldepartments();
+                break;
+            case 'view all roles?':
+                viewAllroles();
+                break
+            case 'view all employees?':
+                viewAllemployees();
+                break
+            case 'add a department?':
+                adddepartment();
+                break
+            case 'add a role?':
+                addrole();
+                break
+            case 'add an employee?':
+                addemployee();
+                break
+            case 'update an employee role?':
+                updateemployee();
+                break
+        }
+    })
+};
+
+function viewAlldepartments() {
+    const sql = `SELECT * from department`;
+
+    db.query(sql, (err, rows) => {
+        if (err) throw err
+        console.table(rows)
+        main()
+    });
+}
+
+function viewAllroles() {
+    const sql = `SELECT * from role`;
+
+    db.query(sql, (err, rows) => {
+        if (err) throw err
+        console.table(rows)
+        main()
+    });
+}
+
+function viewAllemployees() {
+    const sql = `SELECT * from employee`;
+
+    db.query(sql, (err, rows) => {
+        if (err) throw err
+        console.table(rows)
+        main()
+    });
+}
+
+function adddepartment() {
+    inquirer.prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'Enter your name'
+        },
+    ]).then(function (val) {
+        const sql = `INSERT INTO department (name) VALUES (?)`;
+        const param = [val.name]
+
+        // const sql = `INSERT INTO department SET ?`;
+        // const param = [val]
+
+        db.query(sql, param, (err, rows) => {
+            if (err) throw err
+            console.table(rows)
+            main()
+        });
+
+    })
+}
+
+function addemployee() {
+    const sql = `SELECT * from employee`;
+
+    db.query(sql, (err, rows) => {
+        if (err) throw err
+        console.table(rows)
+        main()
+    });
+}
+
+function addrole() {
+    const sql = `SELECT * from role`;
+
+    db.query(sql, (err, rows) => {
+        if (err) throw err
+        console.table(rows)
+        main()
+    });
+}
+
+function updateemployee() {
+    const sql = `SELECT * from employee`;
+
+    db.query(sql, (err, rows) => {
+        if (err) throw err
+        console.table(rows)
+        main()
+    });
+}
+
